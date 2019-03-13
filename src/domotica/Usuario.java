7ec7092d32a20bc16cb9e5000d7ad9864866b47b
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-13 10:00:03 +0100

    Crea metodo constructor en usuario

--- src/domotica/Usuario.java
+++ src/domotica/Usuario.java
@@ -16,8 +16,12 @@ public class Usuario {
     private String user;
     private String password;
 
-    public Usuario() {
+    public Usuario(String user, String password) {
+        this.user = user;
+        this.password = password;
     }
+
+
 
     public String getUser() {
         return user;
