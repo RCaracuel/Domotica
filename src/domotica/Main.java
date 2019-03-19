3f1efd2c6e3633aa1f76c1acff368469119f63a4
RCaracuel <rosa.caracuel@gmail.com>
2019-03-19 14:25:06 +0100

    Correción del main, bucles y llamadas al método FUNCIONAN

--- src/domotica/Main.java
+++ src/domotica/Main.java
@@ -12,8 +12,28 @@ package domotica;
 public class Main {
     
     public static void main(String[] args) {
+       
+       
+    
+      
+       Centralita tmp= new Centralita();
+       Usuario nuevo;
+       int contador=1;
+        do {
+            
+           nuevo= Vista.login(); 
+           
+            
+           contador++;
+            
+        } while (!(nuevo.equals(tmp.getUsu()))&&contador<=5);
         
-       Vista.menu();
+        if(tmp.comprobacionUsuario(nuevo)){
+            Vista.menu();
+        }else{
+            System.out.println("El usuario o clave es incorrecto");
+        }
+
        //Garaje g1=new Garaje();
        //System.out.println(g1);
        
