8a7b295fc793d5106ee2dd19a574122de50d8b83
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-19 19:20:38 +0100

    Crea metodo de estado de camara, apagar camara y encender camara

--- src/domotica/Camara.java
+++ src/domotica/Camara.java
@@ -10,7 +10,39 @@ package domotica;
  * @author rafa
  */
 public class Camara {
-    
+
     private boolean estado;
     private String ip; // Direccion IP de la camara
+
+    public Camara(boolean estado, String ip) {
+        this.estado = estado;
+        this.ip = ip;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public static String estado(Camara aux) {
+        String est = "";
+        if (aux.isEstado() == true) {
+            est = "La camara está encendida";
+        } else {
+            est = "La camara está apagada";
+        }
+
+        return est;
+    }
 }
