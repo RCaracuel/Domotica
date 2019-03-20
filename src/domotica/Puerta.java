2ae4cbf5b7ffeaa075c887cfb8e91f1e475dc5af
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-20 09:05:49 +0100

    Añade constructores a puerta y metodos de estado, abrir y cerrar

--- src/domotica/Puerta.java
+++ src/domotica/Puerta.java
@@ -14,8 +14,13 @@ public class Puerta {
     private boolean estado;
     private int retardo; // en segundos
 
-    // Java crea un constructor por defecto si no implementamos ninguno
- 
+   public Puerta(boolean estado, int retardo) {
+        this.estado = estado;
+        this.retardo = retardo;
+    }
+
+    public Puerta() {
+    }
 
     public boolean isEstado() {
         return estado;
@@ -36,6 +41,29 @@ public class Puerta {
     @Override
     public String toString() {
         return "Puerta{" + "estado=" + estado + ", retardo=" + retardo + '}';
+    }
+
+    public static String estado(Puerta aux) {
+        String est = "";
+        if (aux.isEstado() == true) {
+            est = "La puerta está abierta";
+        } else {
+            est = "La puerta está cerrada";
+        }
+
+        return est;
+    }
+
+    public static void subirPuerta(Puerta aux) {
+        if (aux.isEstado() == false) {
+            aux.setEstado(true);
+        }
+    }
+
+    public static void bajarPuerta(Puerta aux) {
+        if (aux.isEstado() == true) {
+            aux.setEstado(false);
+        }
     }
     
     
