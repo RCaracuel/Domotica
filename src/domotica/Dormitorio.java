863051ad0baee37b28af6bccee4da0b92c2c856a
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-24 14:11:02 +0100

    Comenta clase dormitorio

--- src/domotica/Dormitorio.java
+++ src/domotica/Dormitorio.java
@@ -11,18 +11,22 @@ package domotica;
  */
 public class Dormitorio extends Habitacion {
     
+    // Atributos
     private Orientacion ori;
    
+    // Constructor parametrizado, donde ademas se llama al constructor de la clase padre
     public Dormitorio(Orientacion ori, Luz luz, Persiana persiana, Camara camara, int m2) {
         super(luz, persiana, camara, m2);
         this.ori = ori;
     }
 
+    // Constructor por defecto
     public Dormitorio() {
         super();
         this.ori=this.ori.SUR;
     }
 
+    // toString
     @Override
     public String toString() {
         return "Dormitorio{" + "ori=" + ori + '}';
