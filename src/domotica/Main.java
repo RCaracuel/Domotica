c20cd3430f17ee881dc53081613b6f4751b80949
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-24 14:13:52 +0100

    Comenta clase main

--- src/domotica/Main.java
+++ src/domotica/Main.java
@@ -10,35 +10,30 @@ package domotica;
  * @author Rosa
  */
 public class Main {
-    
+
     public static void main(String[] args) {
-       
-       
-    
-      
-       Centralita tmp= new Centralita();
-       Usuario nuevo;
-       int contador=1;
+
+        // Creamos la centralita por defecto
+        Centralita tmp = new Centralita();
+        // Creamos el usuario
+        Usuario nuevo;
+        // Contador
+        int contador = 1;
+        // Hacemos un bucle donde el nuevo usuario le haremos el Vista.login() y el contador ira sumando, si este contador
+        // llega a mas de 5 veces y el usuario no es igual al que ya tenemos se cerrará
         do {
-            
-           nuevo= Vista.login(); 
-           
-            
-           contador++;
-            
-        } while (!(nuevo.equals(tmp.getUsu()))&&contador<=5);
-        
-        if(tmp.comprobacionUsuario(nuevo)){
+            nuevo = Vista.login();
+            contador++;
+
+        } while (!(nuevo.equals(tmp.getUsu())) && contador <= 5);
+
+        // Si el usuario es el correcto se ejecutara el menu
+        if (tmp.comprobacionUsuario(nuevo)) {
             Centralita.ejecutarOrden(Vista.menu());
-        }else{
+        // Si no pues nos saltará este mensaje y se apagará
+        } else {
             System.out.println("El usuario o clave es incorrecto");
         }
 
-       //Garaje g1=new Garaje();
-       //System.out.println(g1);
-       
-       //Garaje g2=new Garaje(2,new Puerta(),50);
-       //System.out.println(g2);
-       
     }
 }
