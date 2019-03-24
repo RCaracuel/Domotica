e8a00c31d4e7d443386853031185559fd6aaf105
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-24 13:35:32 +0100

    Implenta en el metodo de encenderCamara() las condiciones con las situaciones expuestas en el proyecto

--- src/domotica/Camara.java
+++ src/domotica/Camara.java
@@ -5,6 +5,8 @@
  */
 package domotica;
 
+import java.time.LocalTime;
+
 /**
  *
  * @author rafa
@@ -20,10 +22,8 @@ public class Camara {
     }
 
     public Camara() {
-        this.estado=false;
+        this.estado = false;
     }
-    
-    
 
     public boolean isEstado() {
         return estado;
@@ -53,8 +53,28 @@ public class Camara {
     }
 
     public static void encenderCamara(Camara aux) {
-        if (aux.isEstado() == false) {
-            aux.setEstado(true);
+        LocalTime x = LocalTime.of(20, 00);
+        LocalTime y = LocalTime.of(8, 00);
+        LocalTime z = LocalTime.of(18, 00);
+        LocalTime ahora = LocalTime.now();
+        Salon tmp;
+        Dormitorio tmp2;
+
+        if (ahora.isAfter(x) && ahora.isBefore(y)) {
+            tmp.getLuz().setEstado(true);
+            tmp.getPersiana().setEstado(EstadoPersiana.SUBIDA);
+        } else if (ahora.isAfter(y) && ahora.isBefore(z)
+                && tmp.getPersiana().getEstado(EstadoPersiana.BAJADA)) {
+            tmp.getLuz().setEstado(true);
+        }
+
+        if (ahora.isAfter(x) && ahora.isBefore(y)) {
+            tmp2.getLuz().setEstado(true);
+            tmp2.getPersiana().setEstado(EstadoPersiana.SUBIDA);
+
+        } else if (ahora.isAfter(y) && ahora.isBefore(z)
+                && tmp2.getPersiana().getEstado(EstadoPersiana.BAJADA)) {
+            tmp2.getLuz().setEstado(true);
         }
     }
 
@@ -68,7 +88,5 @@ public class Camara {
     public String toString() {
         return "Camara{" + "estado=" + estado + ", ip=" + ip + '}';
     }
-    
-    
-    
+
 }
