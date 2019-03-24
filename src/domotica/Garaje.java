df2bbe6fefd751f8ee26716ff541867772714422
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-24 14:11:45 +0100

    Comenta clase garaje

--- src/domotica/Garaje.java
+++ src/domotica/Garaje.java
@@ -12,10 +12,12 @@ package domotica;
 // La clase garaje es un subtipo o subclase de estancia. Garaje extiende a estancia o garaje hereda de estancia
 public class Garaje extends Estancia {
 
+    // Atributos
     private int capacidadVehiculos;
     private Puerta puertaAutomatica;
 
     // Los constructores son los unicos metodos que no se heredan
+    // Constructor parametrizado
     public Garaje(int capacidadVehiculos, Puerta puertaAutomatica, int m2) {
         // Llamada al constructor de la clase padre
         super(m2);
@@ -23,12 +25,14 @@ public class Garaje extends Estancia {
         this.puertaAutomatica = puertaAutomatica;
     }
 
+    // Constructor por defecto
     public Garaje() {
         super(); // Crea la estancia con m2=0
         this.capacidadVehiculos = 0;
         this.puertaAutomatica = new Puerta();
     }
 
+    // Getters y setters
     public int getCapacidadVehiculos() {
         return capacidadVehiculos;
     }
@@ -47,7 +51,7 @@ public class Garaje extends Estancia {
 
     // Ejemplo de acceso a metodo heredado usando el puntero super, que apunta a la clase padre de la clase
     // en la que estoy
-    
+    // toString
     @Override
     public String toString() {
         return super.toString() + "\nGaraje{" + "capacidadVehiculos=" + capacidadVehiculos + ",\n" + puertaAutomatica + '}';
