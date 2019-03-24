164735c99a64dc4dbc5be2f0ec817fa5a6bbd124
Rafa <rafaelcarrillobonilla@gmail.com>
2019-03-24 14:15:04 +0100

    Comenta clase salon

--- src/domotica/Salon.java
+++ src/domotica/Salon.java
@@ -11,16 +11,20 @@ package domotica;
  */
 public class Salon extends Habitacion {
 
+    // Atributos
     private boolean terraza;
 
+    // Constructor parametrizado
     public Salon(boolean terraza, Luz luz, Persiana persiana, Camara camara, int m2) {
         super(luz, persiana, camara, m2);
         this.terraza = terraza;
     }
 
+    // Constructor por defecto
     public Salon() {
     }
 
+    // Getters y setters
     public boolean isTerraza() {
         return terraza;
     }
@@ -29,6 +33,7 @@ public class Salon extends Habitacion {
         this.terraza = terraza;
     }
 
+    // toString
     @Override
     public String toString() {
         return "Salon{" + "terraza=" + terraza + '}';
